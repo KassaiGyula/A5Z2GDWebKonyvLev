ef14ace52199a425258ebf46e90da015d87bb278
Gyula Kassai <162285198+KassaiGyula@users.noreply.github.com>
2024-04-04 12:35:06 +0200

    Javítva

--- A5Z2GD_03_08/script.js
+++ A5Z2GD_03_08/script.js
@@ -10,11 +10,40 @@ $(document).ready(function()
         $("ul li:first-child").hide("slow");
         $("[href]").hide("slow");
         });
+    $(fejlec).mouseleave(function()
+        {
+        alert("Információ");
+        //$(p1).hide("slow");
+        });
+    $(".input1").hover(function()
+        {
+        $(".input1").css('border', '2px solid red');
+        },
+    function()
+        {
+        $(".input1").css('border', '2px solid gray');
+        });
+    $(".input2").hover(function()
+        {
+        $(".input2").css('border', '2px solid red');
+        },
+    function()
+        {
+        $(".input2").css('border', '2px solid gray');
+        });
+    $(".input3").hover(function()
+        {
+        $(".input3").css('border', '2px solid red');
+        },
+    function()
+        {
+        $(".input3").css('border', '2px solid gray');
+        });
     $(k1).click(function()
         {
         $(p1).hide("slow");
         });
-    $(k1).dblclick(function()
+    $(k2).dblclick(function()
         {
         $(p2).hide("slow");
         });
